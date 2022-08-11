6b20171b40530bbb4b784647bb11401567d9b67d
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2022-08-11 12:50:40 +0800

    Update ex3_1a.cpp

--- ch_03/ex3_1a.cpp
+++ ch_03/ex3_1a.cpp
@@ -70,3 +70,55 @@ int main(int argc, char *argv[])
 }
 
 // use `using` for 2.6.2
+
+#include <iostream>
+#include <string>
+
+using std::cin; using std::cout; using std::endl; using std::string;
+
+class Sales_data
+{
+public:
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+int main(int argc, char *argv[])
+{
+    Sales_data total, currData;
+    // 保存计数
+    int cnt = 1;
+    double price = 0.0;
+    // 保存第一个Sales_data
+    if (cin >> total.bookNo >> total.units_sold >> price)
+    {
+        total.revenue = total.units_sold * price;
+        // 循环读入
+        while (cin >> currData.bookNo >> currData.units_sold >> price)
+        {
+            currData.revenue = currData.units_sold * price;
+            if (currData.bookNo == total.bookNo)
+            {
+                total.units_sold += currData.units_sold;
+                total.revenue += currData.revenue;
+                ++cnt;
+            }
+            else
+            {
+                cout << total.bookNo << " occur " << cnt << " times " << endl;
+                cout << total.bookNo << ' ' << total.units_sold << ' ' << total.revenue << ' ';
+                if (total.units_sold != 0)
+                    cout << total.revenue / total.units_sold << endl;
+                total.bookNo = currData.bookNo;
+                total.revenue = currData.revenue;
+                total.units_sold = currData.units_sold;
+            }
+        }
+        cout << total.bookNo << ' ' << total.units_sold << ' ' << total.revenue << ' ';
+        if (total.units_sold != 0)
+            cout << total.revenue / total.units_sold << endl;
+        return 0;
+    }
+    return -1;
+}
