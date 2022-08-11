6f324ed5eb099fc49b34ce1656a8cdb9add53133
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2022-08-11 12:42:49 +0800

    Update ex3_1a.cpp

--- ch_03/ex3_1a.cpp
+++ ch_03/ex3_1a.cpp
@@ -1,19 +1,69 @@
 // use `using` for 1.4.1
 
+ex1_9
+// sum the numbers from 50 to 100 (use while)
 #include <iostream>
 
-using std::cin;
-using std::cout;
-using std::endl;
+using std::cout; using std::endl;
 
-int main()
+int main(int argc, char *argv[])
 {
-    int sum = 0, val = 1;
-    // keep executing the while as long as val is less than or equal to 10
-    while (val <= 10) {
-        sum += val;
-        ++val;
-    }
-    cout << "Sum of 1 to 10 inclusive is " << sum << endl;
-    return 0;
+	int sum = 0, val = 50;
+	while (val <= 100)
+	{
+		sum += val;
+		++val;
+	}
+	std::cout << "sum of 50 to 100 inclusive is " << sum << std::endl;
+	return 0;
+}
+
+// output: the sum is 3825
+
+ex1_10
+#include <iostream>
+
+using std::cout; using std::endl;
+int main(int argc, char *argv[])
+{
+	int val = 10;
+	while (val >= 0)
+	{
+		std::cout << val << ' ';
+		--val;
+	}
+	std::cout << std::endl;
+	return 0;
+}
+
+ex1_11
+#include <iostream>
+
+using std::cin; using std::cout; using std::endl;
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, a;
+	std::cout << "Please input two integer number: " << std::endl;
+	std::cin >> num1 >> num2;
+	if (num1 >= num2)
+	{
+		a = num2;
+		while (a <= num1)
+		{
+			std::cout << a << ' ';
+			++a;
+		}
+	}
+	if (num1 < num2)
+	{
+		a = num1;
+		while (a <= num2)
+		{
+			std::cout << a << ' ';
+			++a;
+		}
+	}
+	std::cout << std::endl;
+	return 0;
 }
