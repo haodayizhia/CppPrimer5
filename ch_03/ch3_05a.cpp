075f3612159f95882e56a9733362b841a33d6a05
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2022-08-13 17:13:18 +0800

    Create ch3_05a.cpp

---
+++ ch_03/ch3_05a.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char *argv[])
+{
+	string str, largeStr;
+	if (cin >> str)
+	{
+		largeStr = str;
+		while (cin >> str)
+		{
+			largeStr += str;
+		}
+		cout << largeStr << endl;
+		return 0;
+	}
+	cerr << "input error" << endl;
+	return -1;
+}
