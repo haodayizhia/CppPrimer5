f1dae51d2226e8a38274676be3e9d1294b1f0652
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2022-08-18 23:33:27 +0800

    Update ex3_16.cpp

--- ch_03/ex3_16.cpp
+++ ch_03/ex3_16.cpp
@@ -36,3 +36,95 @@ int main(int argc, char *argv[])
 	print(v7);
 	return 0;
 }
+
+// 高级版本
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::make_shared;
+using std::shared_ptr;
+using std::string;
+using std::vector;
+
+class vec
+{
+public:
+	vec() = default;
+	virtual void print() const {};
+	virtual ~vec() = default;
+};
+
+class vec_int : public vec
+{
+public:
+	vec_int() = default;
+	vec_int(const vector<int> &ini) : vi(ini)
+	{
+	}
+	void print() const override
+	{
+		cout << "size:" << vi.size() << ' ' << "value:";
+		for (auto &i : vi)
+		{
+			cout << i << ' ';
+		}
+		cout << endl;
+	}
+
+private:
+	vector<int> vi = {};
+};
+
+class vec_str : public vec
+{
+public:
+	vec_str();
+	vec_str(const vector<string> &ini) : vs(ini)
+	{
+	}
+	void print() const override
+	{
+		cout << "size:" << vs.size() << ' ' << "value:";
+		for (auto &i : vs)
+		{
+			cout << i << ' ';
+		}
+		cout << endl;
+	}
+
+private:
+	vector<string> vs;
+};
+
+int main(int argc, char *argv[])
+{
+	int count = 1;
+	vector<shared_ptr<vec>> vv;
+	shared_ptr<vec_int> v1 = make_shared<vec_int>(vector<int>());
+	vv.push_back(v1);
+	shared_ptr<vec_int> v2 = make_shared<vec_int>(vector<int>(10));
+	vv.push_back(v2);
+	shared_ptr<vec_int> v3 = make_shared<vec_int>(vector<int>(10, 42));
+	vv.push_back(v3);
+	shared_ptr<vec_int> v4 = make_shared<vec_int>(vector<int>{10});
+	vv.push_back(v4);
+	shared_ptr<vec_int> v5 = make_shared<vec_int>(vector<int>{10, 42});
+	vv.push_back(v5);
+	shared_ptr<vec_str> v6 = make_shared<vec_str>(vector<string>{10});
+	vv.push_back(v6);
+	shared_ptr<vec_str> v7 = make_shared<vec_str>(vector<string>{10, "hi"});
+	vv.push_back(v7);
+	for (auto i : vv)
+	{
+		cout << 'v' << count << ':';
+		i->print();
+		++count;
+	}
+	return 0;
+}
