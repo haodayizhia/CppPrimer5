e11cd96bf632326f55934c6db1e3e03a4c96fb16
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2022-08-25 15:30:42 +0800

    Create ex3_40.cpp

---
+++ ch_03/ex3_40.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	char c1[] = "w";
+	char c2[] = "hy";
+	strcat(c1, c2);
+	// c3不能为空?
+	char c3[] = " ";
+	strcpy(c3, c1);
+	for (size_t i = 0; i <= strlen(c3); ++i)
+		cout << c3[i];
+	cout << endl;
+	cout << "c3 size: " << strlen(c3) << endl;
+	return 0;
+}
