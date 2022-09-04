18589169dc307fa94ae7de71d556f75e664d3f8c
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2022-09-04 22:44:12 +0800

    Update ex3_40.cpp

--- ch_03/ex3_40.cpp
+++ ch_03/ex3_40.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	char c2[] = "hy";
 	strcat(c1, c2);
 	// c3不能为空?
-	char c3[] = " ";
+	char c3[4];
 	strcpy(c3, c1);
 	for (size_t i = 0; i <= strlen(c3); ++i)
 		cout << c3[i];
