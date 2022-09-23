44ab3b291ef8268de1bf2925b8ac2a7c94e2cfa2
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2022-09-23 08:31:48 +0800

    Create ex4_28.cpp

---
+++ ch_04/ex4_28.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	cout << sizeof(wchar_t) << endl;
+	cout << sizeof(char16_t) << endl;
+	cout << sizeof(char32_t) << endl;
+	cout << sizeof(short) << endl;
+	cout << sizeof(int) << endl;
+	cout << sizeof(int *) << endl;
+	cout << sizeof(double) << endl;
+	cout << sizeof(long double) << endl;
+	cout << sizeof(double *) << endl;
+	cout << sizeof(char) << endl;
+	cout << sizeof(char *) << endl;
+	return 0;
+}
