162804ce78ba9eee1362a08f07ae66941a63b5a9
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2023-03-21 17:16:14 +0800

    Create ex6_25_26.cpp

---
+++ ch_06/ex6_25_26.cpp
@@ -0,0 +1,11 @@
+// Type your code here, or load an example.
+
+using namespace std;
+
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char *argv[]) {
+    string s = strcat(argv[1], argv[2]);
+    cout << s << endl;
+}
