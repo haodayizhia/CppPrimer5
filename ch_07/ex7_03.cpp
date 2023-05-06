245ea5b23c9a0f12415a7f17a943c7235a3542b0
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2023-05-06 16:45:12 +0800

    Create ex7_03.cpp

---
+++ ch_07/ex7_03.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Sales_data {
+    string isbn() const { return bookNo; }
+    Sales_data &combine(const Sales_data &);
+    double avg_price() const;
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+Sales_data &Sales_data::combine(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+double Sales_data::avg_price() const {
+    if (units_sold)
+        return revenue / units_sold;
+    else
+        return 0;
+}
+istream &read(istream &is, Sales_data &item) {
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = item.units_sold * price;
+    return is;
+}
+ostream &print(ostream &os, const Sales_data &item) {
+    os << item.isbn() << ' ' << item.units_sold << ' ' << item.revenue << ' '
+       << item.avg_price();
+    return os;
+}
+
+int main(int argc, char *argv[]) {
+    Sales_data total;
+    if (read(cin, total)) {
+        Sales_data trans;
+        while (read(cin, trans)) {
+            if (total.isbn() == trans.isbn())
+                total.combine(trans);
+            else {
+                print(cout, total) << endl;
+                total = trans;
+            }
+        }
+        print(cout, total) << endl;
+    } else {
+        cerr << "No data?!" << endl;
+    }
+    return 0;
+}
