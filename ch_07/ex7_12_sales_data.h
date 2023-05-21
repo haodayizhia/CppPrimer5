bec53aef067470c29aa739185cc05607cfcd187d
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2023-05-21 22:06:49 +0800

    Create ex7_12_sales_data.h

---
+++ ch_07/ex7_12_sales_data.h
@@ -0,0 +1,43 @@
+struct Sales_data;
+istream &read(istream &is, Sales_data &item);
+
+struct Sales_data {
+    Sales_data() = default;
+    Sales_data(const string &s) : bookNo(s) {}
+    Sales_data(const string &s, unsigned n, double p)
+        : bookNo(s), units_sold(n), revenue(p * n) {}
+    Sales_data(istream &is){ read(is, *this); };
+    string isbn() const { return bookNo; }
+    Sales_data &combine(const Sales_data &);
+    double avg_price() const;
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+Sales_data &Sales_data::combine(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+double Sales_data::avg_price() const {
+    if (units_sold)
+        return revenue / units_sold;
+    else
+        return 0;
+}
+istream &read(istream &is, Sales_data &item) {
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = item.units_sold * price;
+    return is;
+}
+ostream &print(ostream &os, const Sales_data &item) {
+    os << item.isbn() << ' ' << item.units_sold << ' ' << item.revenue << ' '
+       << item.avg_price();
+    return os;
+}
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
