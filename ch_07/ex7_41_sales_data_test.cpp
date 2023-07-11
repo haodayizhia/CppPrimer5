1f1d4e10761eb85c3c98400ab3aa6601bcc93353
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2023-07-11 16:33:30 +0800

    Create ex7_41_sales_data_test.cpp

---
+++ ch_07/ex7_41_sales_data_test.cpp
@@ -0,0 +1,33 @@
+#include "ex7_41_sales_data.h"
+
+Sales_data &Sales_data::combine(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+double Sales_data::avg_price() const {
+    if (units_sold)
+        return revenue / units_sold;
+    else
+        return 0;
+}
+std::istream &read(std::istream &is, Sales_data &item) {
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = item.units_sold * price;
+    return is;
+}
+Sales_data::Sales_data(std::istream &is) : Sales_data() {
+    read(is, *this);
+    std::cout << "Sales_data(std::istream &)" << std::endl;
+}
+std::ostream &print(std::ostream &os, const Sales_data &item) {
+    os << item.isbn() << ' ' << item.units_sold << ' ' << item.revenue << ' '
+       << item.avg_price();
+    return os;
+}
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
