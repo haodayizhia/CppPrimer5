90a9752deaf8984642fb5d4a9c8a7dd192fdc674
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2023-07-11 16:35:46 +0800

    Create ex7_41_sales_data_test.cpp

---
+++ ch_07/ex7_41_sales_data_test.cpp
@@ -0,0 +1,10 @@
+#include "ex7_41_sales_data.cpp"
+
+int main(int argc, char *argv[]) {
+    std::cout << "Exercise VERSION " << Exercise_VERSION_MAJOR << '.'
+              << Exercise_VERSION_MINOR << std::endl;
+              Sales_data a1;
+              Sales_data a2("三体",10,20);
+              Sales_data a3(std::cin);
+    return 0;
+}
