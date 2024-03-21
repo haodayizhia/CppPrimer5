c05c1f76e1004c17a80b1b43c47b71e41e5743dc
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2024-03-21 16:08:39 +0800

    Update ex9_18.cpp

--- ch_09/ex9_18.cpp
+++ ch_09/ex9_18.cpp
@@ -5,6 +5,6 @@ int main() {
     std::string wd;
     std::deque<std::string> sd;
     while (std::cin >> wd) sd.push_back(wd);
-    for (auto &i : sd) std::cout << i << ' ';
+    for (auto i = sd.begin(); i != sd.end(); ++i) std::cout << *i << ' ';
     std::cout << std::endl;
 }
