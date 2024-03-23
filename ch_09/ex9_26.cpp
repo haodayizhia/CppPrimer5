5015ad2470e8b6bd320a9106b8afbe260668d235
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2024-03-23 18:01:49 +0800

    Create ex9_26.cpp

---
+++ ch_09/ex9_26.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+#include "ExerciseConfig.h"
+
+int main(int argc, char *argv[]) {
+    std::cout << "Exercise VERSION " << Exercise_VERSION_MAJOR << '.'
+              << Exercise_VERSION_MINOR << std::endl;
+    int ia[] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 55, 89};
+    std::vector<int> iv(std::begin(ia), std::end(ia));
+    std::list<int> il(std::begin(ia), std::end(ia));
+    for (auto it = iv.begin(); it != iv.end();) {
+        if (!(*it & 1))
+            it = iv.erase(it);
+        else
+            ++it;
+    }
+    for (auto &i : iv) std::cout << i << ' ';
+    std::cout << std::endl;
+    for (auto it = il.begin(); it != il.end();) {
+        if (*it & 1)
+            it = il.erase(it);
+        else
+            ++it;
+    }
+    for (auto &i : il) std::cout << i << ' ';
+    std::cout << std::endl;
+    return 0;
+}
