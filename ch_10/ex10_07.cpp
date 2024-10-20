fdfbb5e5b4d05448a44d612ddee7be10d99fbd5c
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2024-10-20 20:30:53 +0800

    Create ex10_07.cpp

---
+++ ch_10/ex10_07.cpp
@@ -0,0 +1,5 @@
+void elimDups(std::vector<std::string> &words) {
+    std::sort(words.begin(), words.end());
+    auto end_unique = std::unique(words.begin(), words.end());
+    words.erase(end_unique, words.end());
+}
