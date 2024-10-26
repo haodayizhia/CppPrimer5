2273cbb019eb943f27d4bdb27c1e328049053361
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2024-10-26 17:17:05 +0800

    Create ex10_25.cpp

---
+++ ch_10/ex10_25.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using std::placeholders::_1;
+
+std::ostream &print(const std::vector<std::string> &words) {
+    for (const auto &i : words) std::cout << i << ' ';
+    return std::cout << std::endl;
+}
+void elimDups(std::vector<std::string> &words) {
+    std::sort(words.begin(), words.end());
+    print(words);
+    auto end_unique = std::unique(words.begin(), words.end());
+    print(words);
+    words.erase(end_unique, words.end());
+    print(words);
+}
+std::string make_plural(size_t ctr, const std::string &word,
+                        const std::string &ending) {
+    return ctr > 1 ? word + ending : word;
+}
+bool checksize(const std::string &s, std::string::size_type sz) {
+    return s.size() > sz;
+}
+void biggies(std::vector<std::string> words,
+             std::vector<std::string>::size_type sz) {
+    elimDups(words);
+    std::stable_sort(words.begin(), words.end(),
+                     [](const std::string &s1, const std::string &s2) {
+                         return s1.size() < s2.size();
+                     });
+    auto wc =
+        std::find_if(words.begin(), words.end(), std::bind(checksize, _1, sz));
+    auto count = words.end() - wc;
+    std::cout << count << ' ' << make_plural(count, "word", "s")
+              << " of length " << sz << " or longer" << std::endl;
+    std::for_each(wc, words.end(),
+                  [](const std::string &s) { std::cout << s << ' '; });
+    std::cout << std::endl;
+}
+int main(int argc, char *argv[]) {
+    std::vector<std::string> words = {"the",  "quick", "red", "fox",   "jumps",
+                                      "over", "slow",  "red", "turtle"};
+    print(words);
+    biggies(words, 4);
+    return 0;
+}
