90bd49774dc6e973ceaa63466b5d93d0ea9add8d
haodayizhia <50456094+haodayizhia@users.noreply.github.com>
2024-12-02 22:30:24 +0800

    Update ex12_02.h

--- ch_12/ex12_02.h
+++ ch_12/ex12_02.h
@@ -2,49 +2,47 @@
 #include <memory>
 #include <vector>
 
-class strBlob {
+class StrBlob {
    public:
     typedef std::vector<std::string>::size_type size_type;
-    strBlob();
-    strBlob(std::initializer_list<std::string> il);
+    StrBlob();
+    StrBlob(std::initializer_list<std::string> il);
     size_type size() const { return data->size(); };
     bool empty() const { return data->empty(); };
     void push_back(const std::string &t) { data->push_back(t); };
     void pop_back();
     std::string &front();
     std::string &back();
-    const std::string &front();
-    const std::string &back();
+    std::string &front() const;
+    std::string &back() const;
 
    private:
     std::shared_ptr<std::vector<std::string>> data;
     void check(size_type i, const std::string &msg) const;
 };
-strBlob::strBlob() : data(std::make_shared<std::vector<std::string>>()) {}
-strBlob::strBlob(std::initializer_list<std::string> il)
+StrBlob::StrBlob() : data(std::make_shared<std::vector<std::string>>()) {}
+StrBlob::StrBlob(std::initializer_list<std::string> il)
     : data(std::make_shared<std::vector<std::string>>(il)) {}
-void strBlob::check(size_type i, const std::string &msg) const {
+void StrBlob::check(size_type i, const std::string &msg) const {
     if (i >= data->size()) throw std::out_of_range(msg);
 }
-std::string &strBlob::front() {
-    check(0, "front on empty strBlob");
+std::string &StrBlob::front() {
+    check(0, "front on empty StrBlob");
     return data->front();
 }
-const std::string &strBlob::front() {
-    check(0, "front on empty strBlob");
+std::string &StrBlob::front() const {
+    check(0, "front on empty StrBlob");
     return data->front();
 }
-std::string &strBlob::back() {
-    check(0, "back on empty strBlob");
+std::string &StrBlob::back() {
+    check(0, "back on empty StrBlob");
     return data->back();
 }
-const std::string &strBlob::back() {
-    check(0, "back on empty strBlob");
+std::string &StrBlob::back() const {
+    check(0, "back on empty StrBlob");
     return data->back();
 }
-void strBlob::pop_back() {
-    check(0, "pop_back on empty strBlob");
+void StrBlob::pop_back() {
+    check(0, "pop_back on empty StrBlob");
     data->pop_back();
 }
-
-int main(int argc, char *argv[]) {}
